877fd2cdab34d37cbdddd816e49fce8d61264cfc
Manuel Baumgartner <manu.baumi@outlook.com>
2023-06-27 16:04:44 +0200

    Add database framework

---
+++ util/json.ts
@@ -0,0 +1,13 @@
+// import sjson from 'secure-json-parse';
+
+// export function parseJson(
+// stringifiedJson: string,
+// ): undefined | CookieCommentItem[] {
+//  if (!stringifiedJson) return undefined;
+//
+//  try {
+//    return sjson(stringifiedJson);
+//  } catch {
+//    return undefined;
+//  }
+// }
